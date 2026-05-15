6562498feb7c01438120b8d2204a5c5eaeb7fe45
Jingyi Jolie Moh <s5420621@student.ad.griffith.edu.au>
2026-05-15 17:01:58 +1000

    created raylib plugin

---
+++ Plugins/RaylibPCGMap/Source/RaylibPCGMap/Private/RaylibPCGMapBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RaylibPCGMapBPLibrary.h"
+#include "RaylibPCGMap.h"
+
+URaylibPCGMapBPLibrary::URaylibPCGMapBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float URaylibPCGMapBPLibrary::RaylibPCGMapSampleFunction(float Param)
+{
+	return -1;
+}
+
